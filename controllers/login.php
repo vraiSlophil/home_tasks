32a9434348311a7ecffab20917d8777b3927d535
vraiSlophil <slophil.contact@gmail.Com>
2023-09-17 15:53:58 +0200

    login logic

--- controllers/login.php
+++ controllers/login.php
@@ -5,7 +5,25 @@ $mediaPath = "../views/media/";
 
 if (isset($_POST["login_username"]) && isset($_POST["login_password"])) {
 
-//    header("Location: index.php");
+    if($_POST["login_username"] == "") {
+        $error = "Veuillez entrer un nom d'utilisateur.";
+    } else if($_POST["login_password"] == "") {
+        $error = "Veuillez entrer un mot de passe.";
+    } else {
+        $user = new User($_POST["login_username"], $_POST["login_password"]);
+        $result = $db->executeQuery("SELECT * FROM users WHERE username = :username AND password = :password", [
+            "username" => $user->getUsername(),
+            "password" => $user->getPassword()
+        ]);
+
+        if (is_array($result) && count($result) > 0) {
+            $_SESSION["user"] = $user;
+            header("Location: index.php");
+            exit();
+        } else {
+            $error = "Nom d'utilisateur ou mot de passe incorrect.";
+        }
+    }
     header("Location: login.php");
     exit();
 }
